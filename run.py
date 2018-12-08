c25df9b9f711af6edb8a407ec4c5428f75a3a352
varunnag <varunnag@amazon.com>
2018-12-08 10:21:29 -0800

    Renamed and reformatted the run.py

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+
+
+files = [
+    ('bash_profile', '.bash_profile'),
+    ('bash', '.bash'),
+    ('gitconfig', '.gitconfig'),
+    ('git-branch-cleanup', 'git-branch-cleanup'),
+    ('inputrc', '.inputrc'),
+    ('tmux.conf', '.tmux.conf'),
+    ('vimrc', '.vimrc'),
+]
+
+CMDS = (
+    'source ~/.bash_profile',
+    'rm -rf ~/.vim',
+    'mkdir -p ~/.vim/bundle/',
+    'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim',
+    'vim +PluginInstall +qall'
+)
+
+if __name__ == "__main__":
+    for src, dst in files:
+        src = os.path.abspath(src)
+        dst = os.path.join(os.path.expanduser("~"), dst)
+        if os.path.exists(dst):
+            os.unlink(dst)
+            print("removed: " + dst)
+        os.symlink(src, dst)
+
+    for cmd in CMDS:
+        os.system(cmd)
