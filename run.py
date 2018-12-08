f46286fbf4e637d41a8b457cc383208e11aff598
varunnag <varunnag@amazon.com>
2018-12-08 11:05:52 -0800

    Fixed rm -rf command

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     for src, dst in files:
         src = os.path.abspath(src)
         dst = os.path.join(os.path.expanduser("~"), dst)
-        os.system('rm -rf {}'.format(dst))
+        os.system('rm -rf ' + dst)
         os.symlink(src, dst)
 
     for cmd in CMDS:
