8405bb986238378afbd80fee621effb8a4c7bc2f
Nag Varun Chunduru <cnv1989@gmail.com>
2020-08-12 10:14:45 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOTFILES=('bashrc' 'bash' 'gitconfig' 'inputrc' 'tmux.conf' 'vimrc')
+DOTFILES=('bash' 'gitconfig' 'inputrc' 'tmux.conf' 'vimrc')
 
 for dotfile in ${DOTFILES[@]}; do
     if [[ -e ${HOME}/.${dotfile} ]]; then
@@ -10,17 +10,3 @@ for dotfile in ${DOTFILES[@]}; do
     echo "Create symlink ${HOME}/.${dotfile}"
     ln -s ${PWD}/${dotfile} ${HOME}/.${dotfile}
 done
-
-
-if [[ -e ${HOME}/.bash_profile ]]; then
-    echo "Removing ${HOME}/.bash_profile"
-    rm ${HOME}/.bash_profile
-fi
-echo "Creating ${HOME}/.bash_profile"
-touch ${HOME}/.bash_profile
-cat > ${HOME}/.bash_profile << EOM
-if [ -f ${HOME}/.bashrc ]; then
-    source ${HOME}/.bashrc
-fi
-EOM
-source ${HOME}/.bashrc
